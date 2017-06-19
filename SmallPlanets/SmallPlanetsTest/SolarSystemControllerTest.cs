6585d763afcc8e21e4097966b46e8803ff4bcf73
James <jamesprivett@hotmail.co.uk>
2017-06-19 15:42:49 +0100

    SmallPlanetsTest added.
    Unit test project added.

---
+++ SmallPlanets/SmallPlanetsTest/SolarSystemControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmallPlanetsTest
+{
+    [TestClass]
+    public class SolarSystemControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
