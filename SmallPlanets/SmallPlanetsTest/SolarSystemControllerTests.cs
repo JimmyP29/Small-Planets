54b187c32bec4c767467813d7b0d475a3996953e
James <jamesprivett@hotmail.co.uk>
2017-06-21 12:29:56 +0100

    SmallPlanetsTest project created
    Test project created, Moq installed

---
+++ SmallPlanets/SmallPlanetsTest/SolarSystemControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SmallPlanets.Controllers;
+using SmallPlanets.Interfaces;
+using System.Web.Mvc;
+
+namespace SmallPlanetsTest
+{
+    [TestClass]
+    public class SolarSystemControllerTests
+    {
+        [TestMethod]
+        public void Index_Loads_SolarSystem_Into_Session()
+        {
+            // Arrange
+            var mockApiConsumer = new Mock<IAPIConsumer>(MockBehavior.Strict);
+            var mockSolarSystem = new Mock<ISolarSystem>(MockBehavior.Strict);
+            SolarSystemController controller = new SolarSystemController(mockApiConsumer.Object, mockSolarSystem.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
