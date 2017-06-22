05b517f84f85e4c5e2785c9fe0a3003e35e8d770
James <jamesprivett@hotmail.co.uk>
2017-06-22 16:57:29 +0100

    Test project for PlanetAPI added.
    References setup (Including Moq).

---
+++ SmallPlanets/PlanetAPITest/PlanetBuilderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanetAPI.Enums;
+using PlanetAPI.Services;
+using System.Drawing;
+
+namespace PlanetAPITest
+{
+    [TestClass]
+    public class PlanetBuilderServiceTests
+    {
+        [TestMethod]
+        public void GetImage_Returns_Bitmap_Earth()
+        {
+            PlanetBuilderService service = new PlanetBuilderService();
+            Bitmap result = service.GetImage(PlanetEnum.Earth);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
