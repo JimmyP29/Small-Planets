6a13936aa954cc156dab9b72a94fc5be852f153c
James <jamesprivett@hotmail.co.uk>
2017-07-01 13:17:20 +0100

    Added Selenium test project
    Added Selenium test project

---
+++ SmallPlanets/SmallPlanetsTestSelenium/CaptainsLogTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmallPlanetsTestSelenium
+{
+    [TestClass]
+    public class CaptainsLogTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
