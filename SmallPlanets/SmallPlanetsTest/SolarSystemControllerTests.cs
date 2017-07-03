845bd448a2aaec1b6a10686a99730a5640b5455f
James <jamesprivett@hotmail.co.uk>
2017-07-03 11:24:04 +0100

    Cleaned up code
    Cleaned up code

--- SmallPlanets/SmallPlanetsTest/SolarSystemControllerTests.cs
+++ SmallPlanets/SmallPlanetsTest/SolarSystemControllerTests.cs
@@ -14,63 +14,6 @@ namespace SmallPlanetsTest
     public class SolarSystemControllerTests
     {
         [TestMethod]
-        public void Index_Loads_SolarSystem_Into_Session()
-        {
-            ////// Arrange
-            ////var mockApiConsumer = new Mock<IAPIConsumer>(MockBehavior.Strict);
-            ////var mockSolarSystem = new Mock<ISolarSystem>(MockBehavior.Strict);
-            ////SolarSystemController controller = new SolarSystemController(mockApiConsumer.Object, mockSolarSystem.Object);
-
-            ////// Act
-            ////ViewResult result = controller.Index() as ViewResult;
-
-            ////// Assert
-            ////Assert.IsNotNull(result);
-            //// Arrange
-            //var mockApiConsumer = new Mock<IAPIConsumer>(MockBehavior.Strict);
-            //var mockSolarSystem = new Mock<ISolarSystem>(MockBehavior.Strict);
-            //var mockControllerContext = new Mock<ControllerContext>();
-            //var mockSession = new Mock<HttpSessionStateBase>();
-
-            //IAPIConsumer apiConsumer = new APIConsumerService();
-            //ISolarSystem service = new SolarSystemService(apiConsumer);
-
-            //mockSession.SetupGet(s => s["SolarSystem"]).Returns(new SolarSystem(service));
-            //mockControllerContext.Setup(p => p.HttpContext.Session).Returns(mockSession.Object);
-            //SolarSystemController controller = new SolarSystemController(apiConsumer, service);
-            //controller.ControllerContext = mockControllerContext.Object;
-            //controller.Index();
-
-            ////mockSession.SetupGet(s => s["SolarSystem"]).Returns(new SolarSystem(mockSolarSystem.Object));
-            ////mockControllerContext.Setup(p => p.HttpContext.Session).Returns(mockSession.Object);
-            ////SolarSystemController controller = new SolarSystemController(mockApiConsumer.Object, mockSolarSystem.Object);
-            ////controller.ControllerContext = mockControllerContext.Object;
-
-            //// Act
-            //// ViewResult result = controller.Index() as ViewResult;
-
-            //// Assert
-            ////Assert.IsNotNull(controller.ControllerContext);
-            ////  SolarSystem correct = new SolarSystem(mockSolarSystem.Object);
-            //SolarSystem correct = new SolarSystem(service);
-            ////correct.CreateSolarSystem();
-            //SolarSystem actual = (SolarSystem)controller.Session["SolarSystem"];
-            //Assert.AreEqual(correct.Planets.Count, actual.Planets.Count);
-            Mock<IAPIConsumer> mockApiConsumer = new Mock<IAPIConsumer>();
-            Mock<ISolarSystem> mockSolarSystem = new Mock<ISolarSystem>();
-
-            SolarSystemController controller = new SolarSystemController(mockApiConsumer.Object, mockSolarSystem.Object);
-            controller.ControllerContext = SetupSessionWithReturnValue(null).Object;
-            controller.Index();
-
-            SolarSystem correct = new SolarSystem(mockSolarSystem.Object);
-            correct.CreateSolarSystem(mockApiConsumer.Object);
-
-            SolarSystem actual = (SolarSystem)controller.Session["SolarSystem"];
-            Assert.AreEqual(correct.Planets.Count, actual.Planets.Count);
-        }
-
-        [TestMethod]
         public void LoadPlanet_Returns_Valid_JSON()
         {
             string planetName = "Earth";
